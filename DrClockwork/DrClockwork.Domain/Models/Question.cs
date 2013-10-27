b09a7e287c7a26b6ad1453b7b4a7793f7db74a2c
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2013-10-27 03:55:24 +0000

    added question model

---
+++ DrClockwork/DrClockwork.Domain/Models/Question.cs
@@ -0,0 +1,12 @@
+﻿namespace DrClockwork.Domain.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Content { get; set; }
+        public string MessageId { get; set; }
+        public string Keyword { get; set; }
+        public string Answer { get; set; }
+    }
+}
